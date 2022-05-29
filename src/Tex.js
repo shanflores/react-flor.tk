be6cda5f117a460d4be79c54aaaa4a0bee2e6a24
angelo28b <101931022+angelo28b@users.noreply.github.com>
2022-05-29 17:32:46 +0800

    first commit

---
+++ src/Tex.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Tex() {
+  return (
+    
+      <div className="tx">
+    <h1>welcome to shan flores landing page! nice</h1>
+    </div>
+  )
+}
